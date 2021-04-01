6c910f7b58f90a2635317577237bc79e16166ea3
lionng <154704230@qq.com>
2021-04-01 23:46:28 +0800

    add new branch

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,14 @@
 module.exports = {
     head: [
-        ['link', { rel: 'icon', href: '/assets/img/logo.png' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],  //https://developer.mozilla.org/zh-CN/docs/Web/Manifest
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: '/assets/img/logo.png' }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/assets/img/logo.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+        ['link', {rel: 'icon', href: '/assets/img/logo.png'}],
+        ['link', {rel: 'manifest', href: '/manifest.json'}],  //https://developer.mozilla.org/zh-CN/docs/Web/Manifest
+        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+        ['link', {rel: 'apple-touch-icon', href: '/assets/img/logo.png'}],
+        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
+        ['meta', {name: 'msapplication-TileImage', content: '/assets/img/logo.png'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
     ],
     markdown: {
         lineNumbers: true
@@ -18,12 +18,7 @@ module.exports = {
     //     indexName: '<INDEX_NAME>'
     // },
     //插件  https://vuepress.vuejs.org/zh/plugin/
-    plugins: ['@vuepress/pwa', '@vuepress/back-to-top', '@vuepress/nprogress', [
-        '@vuepress/google-analytics',
-        {
-            'ga': 'UA-124222542-1' // UA-00000000-0
-        }
-    ]],
+    plugins: ['@vuepress/pwa', '@vuepress/back-to-top', '@vuepress/nprogress'],
     port: '8095',  //项目端口
     //如果你打算将你的网站部署到 https://foo.github.io/bar/，那么 base 的值就应该被设置为 "/bar/"
     base: '/vuepress-starter/',
@@ -44,14 +39,14 @@ module.exports = {
     },
     themeConfig: {
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'lionng/vuepress-starter',
+        // repo: 'lionng/vuepress-starter',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         // repoLabel: '查看源码',
         // 以下为可选的编辑链接选项
-        docsDir: 'docs',
+        // docsDir: 'docs',
         // 默认是 false, 设置为 true 来启用
-        editLinks: true,
+        // editLinks: true,
         // 默认为 "Edit this page"
         // editLinkText: '帮助我们改善此页面！',
 
@@ -101,18 +96,18 @@ module.exports = {
                 // 当前 locale 的 algolia docsearch 选项
                 // algolia: {},
                 nav: [
-                    {text: 'VuePress官网', link: 'https://vuepress.vuejs.org/zh/'},
+                    // {text: 'VuePress官网', link: 'https://vuepress.vuejs.org/zh/'},
                     // {text: '指南', link: '/zh/guide/'},
                     {text: '基础', link: '/zh/base/'},
-                    // {text: '工作', link: '/zh/work/'}
-                    {
-                        text: '了解更多',
-                        items: [
-                            {text: 'hugo博客', link: 'https://lionng.github.io/'},
-                            {text: 'csdn博客', link: 'https://blog.csdn.net/zhang__liuchen'},
-                            {text: 'VuePress官网', link: 'https://vuepress.vuejs.org/zh/'},
-                        ]
-                    }
+                    {text: '工作', link: '/zh/work/'}
+                    // {
+                    //     text: '了解更多',
+                    //     items: [
+                    //         {text: 'hugo博客', link: 'https://lionng.github.io/'},
+                    //         {text: 'csdn博客', link: 'https://blog.csdn.net/zhang__liuchen'},
+                    //         {text: 'VuePress官网', link: 'https://vuepress.vuejs.org/zh/'},
+                    //     ]
+                    // }
                 ],
                 // sidebar: {
                 //     '/zh/guide/': [
